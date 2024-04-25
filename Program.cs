c9a7a104cedcdfdc9bff71af6aa813036d466d5f
Welington <welington.c.campos@hotmail.com>
2024-04-25 08:17:24 -0300

    Adicionando funcionalidades

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+using System;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "modelo 1", imei: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "987654", modelo: "modelo 2", imei: "2222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
